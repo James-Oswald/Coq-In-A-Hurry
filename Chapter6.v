88f1bad9960ea87ddfd4eda97d3e8420d639bff1
James Oswald <jamesoswald111@gmail.com>
2022-11-25 14:10:59 -0500

    Chapter 6.

--- Chapter6.v
+++ Chapter6.v
@@ -19,9 +19,36 @@ Fixpoint count n l :=
     | nil => 0
     | a::tl => let r := count n tl in if n =? a then 1 + r else r
     end.
-    
+
+(*Me proving that sort works*)
+Lemma sort_example: sort (4::3::2::1::nil) = (1::2::3::4::nil).
+Proof.
+    unfold sort.
+    simpl (insert 1 nil).
+    simpl (insert 2 _).
+    simpl (insert 3 _).
+    simpl (insert 4 _).
+    reflexivity.
+Qed.
+
 Lemma insert_incr : forall n l, count n (insert n l) = 1 + count n l.
 Proof.
     intros n l.
     induction l.
+    simpl count.
+    rewrite Nat.eqb_refl.
+    ring.
+
     simpl.
+    case (n <=? a).
+    simpl.
+    rewrite Nat.eqb_refl.
+    reflexivity.
+    simpl.
+    case (n =? a).
+    rewrite IHl.
+    ring.
+    rewrite IHl.
+    ring.
+Qed.
+
